cf60e0dbe48c937efcedeaea94f5e34d2f540bc4
David Vendel <33971649+justdvl@users.noreply.github.com>
2020-03-29 13:53:29 +0100

    languageTo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import axios from "axios";
-import { LOGOUT_USER } from "../config/endpoints";
+import {
+  LOGOUT_USER,
+  GET_LANGUAGE_TO,
+  SET_LANGUAGE_TO
+} from "../config/endpoints";
 import styled from "styled-components";
 import Cookies from "universal-cookie";
 
@@ -18,8 +22,8 @@ const Templates = styled(Button)`
   margin-right: 5px;
 `;
 
-const Users = styled(Button)`
-  float: left;
+const Language = styled.select`
+  margin-bottom: 2px;
 `;
 
 const Logout = styled(Button)`
@@ -68,10 +72,82 @@ async function logoutCall(doLogout) {
     });
 }
 
+const getLanguageTo = async () => {
+  const url = GET_LANGUAGE_TO;
+  const cookies = new Cookies();
+  const data = { auth: cookies.get("userToken") };
+
+  const ret = await axios
+    .post(url, data, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      withCredentials: true
+    })
+    .then(response => {
+      console.log("getLanguageTo call success", response.data.languageTo);
+      return response.data.languageTo;
+    })
+    .catch(error => {
+      console.log("getLanguageTo call error");
+      return false;
+    });
+
+  return ret;
+};
+
 // makes a logout available for every route (for logged in users)
 function Header({ properties, Component, doLogout }) {
+  console.log("Header rerender");
   const menuClick = direction => {
     properties.history.push(direction);
+  };
+
+  const languagesTo = [
+    { symbol: "fr", name: "Fr" },
+    { symbol: "de", name: "De" },
+    { symbol: "es", name: "Es" },
+    { symbol: "sk", name: "Sk" },
+    { symbol: "cz", name: "Cz" }
+  ];
+
+  const [currentLanguageTo, setCurrentLanguageTo] = React.useState("fr");
+
+  async function fetchMyAPI() {
+    let languageTo = await getLanguageTo();
+    console.log("languagetTo", languageTo);
+    setCurrentLanguageTo(languageTo);
+  }
+
+  useEffect(() => {
+    fetchMyAPI();
+  }, []);
+
+  const changedLanguagesTo = async e => {
+    console.log("e", e.target.value);
+    const cookies = new Cookies();
+
+    const data = {
+      auth: cookies.get("userToken"),
+      languageTo: e.target.value
+    };
+    const url = SET_LANGUAGE_TO;
+
+    const ret = await axios
+      .post(url, data, {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true
+      })
+      .then(() => {
+        console.log("language to call success");
+        return true;
+      })
+      .catch(error => {
+        console.log("language to call error");
+        return false;
+      });
   };
 
   return (
@@ -84,14 +160,23 @@ function Header({ properties, Component, doLogout }) {
         >
           Home
         </Templates>
-        <Users
-          onClick={() => {
-            menuClick("/users");
-          }}
-        >
-          Users
-        </Users>
-
+        <Language id="language-from">
+          <option value="en">En</option>
+        </Language>
+        =>
+        <Language id="language-to" onChange={changedLanguagesTo}>
+          {languagesTo.map(l => {
+            return (
+              <option
+                key={l.symbol}
+                value={l.symbol}
+                selected={l.symbol == currentLanguageTo ? "selected" : ""}
+              >
+                {l.name}
+              </option>
+            );
+          })}
+        </Language>
         <Logout
           onClick={() => {
             logoutCall(doLogout);
--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,8 +1,8 @@
-const PRODUCTION_MODE = process.env.NODE_ENV === 'production';
+const PRODUCTION_MODE = process.env.NODE_ENV === "production";
 
-let SERVER = 'http://localhost:8000';
-if(PRODUCTION_MODE) {
-    SERVER = '';
+let SERVER = "http://localhost:8000";
+if (PRODUCTION_MODE) {
+  SERVER = "";
 }
 
 export const SERVER_URL = SERVER;
@@ -11,6 +11,9 @@ export const USER_LOGGED = `${SERVER}/user-logged`;
 
 export const USER_LOGIN = `${SERVER}/login`;
 export const LOGOUT_USER = `${SERVER}/logout`;
+
+export const SET_LANGUAGE_TO = `${SERVER}/setLanguageTo`;
+export const GET_LANGUAGE_TO = `${SERVER}/getLanguageTo`;
 
 export const GET_ALL_USERS = `${SERVER}/get-all`;
 export const GET_USER = `${SERVER}/get`;
